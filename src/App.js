d35ddce9a44129b1fbfee9399ea68177d2c24ad7
jindudimuna <danieldimuna@gmail.com>
2022-07-18 20:02:18 +0100

    hero and navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Nav from './pages/Nav';
 import './App.css';
+import React from 'react';
+import Hero from './pages/Hero';
 
 function App() {
   return (
     <div className="App">
     <Nav />
+    <Hero />
     </div>
   );
 }
