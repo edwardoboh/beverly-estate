e60d7f46fca0aabec596179cced619485a6fe5e3
jindudimuna <danieldimuna@gmail.com>
2022-07-19 13:47:31 +0100

    about page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import Nav from './pages/Nav';
 import './App.css';
 import React from 'react';
 import Hero from './pages/Hero';
+import About from './pages/About';
 
 function App() {
   return (
     <div className="App">
     <Nav />
     <Hero />
+    <About />
     </div>
   );
 }
