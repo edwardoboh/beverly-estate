7a813d099ff6cc7e6b4bb4e512d25062d22fef51
Edward O. Oboh <obohedward@gmail>
2022-07-26 21:30:50 +0100

    fix: correct error with card rendering

--- src/components/HouseCards.js
+++ src/components/HouseCards.js
@@ -1,62 +1,49 @@
 import React from "react";
-import housedata from "../housedata";
 
-function Card () {
-    const[data, setData] = React.useState(housedata);
-
-
-function toggle(){
-    setData(prevData =>{
-
-        return{
-            ...prevData, 
-            favorite: !prevData.favorite
-        }
-    })
-};
-
-return(
-    <>
-    {data.map(singleData =>{
-    let favIcon = singleData.favorite ? "../images/favistrue.png" : "../images/favisfalse.png" ;
-
-return(
-
-
-
-    <div className="house-cards" >
-    <div className="card-content">
+function Card ({id, housePic, rent, List, size, site, favorite}) {
     
-        <img src= {`../images/${singleData.housePic}`} className="card-photo" alt="house-view" />
-        <div className="price-div">
-        <span className="price-headings">Monthly rent</span>
-        <span className="price-headings">List Price</span>
-        </div>
- 
-        <div className="prices">
-            <h2 className="rent">{singleData.rent}</h2>
-            <h2 className="list">{singleData.List}</h2>
-            <img src={`..images/${favIcon}`} alt="fav"
-            className='favicon'
-            onClick={()=>toggle(singleData.id)} 
-    
-            />
-    
-        </div>
-    
-        <h2 className="size">{singleData.size}</h2>
-        <h2 className="site">{singleData.site}</h2>
-    </div>
-    
-    </div>
+    const[data, setData] = React.useState(
+        {id, housePic, rent, List, size, site, favorite}
+    );
+    const favIcon = data.favorite ? "../images/favistrue.png" : "../images/favisfalse.png"
+
+    function toggle(){
+        setData(prevData =>{
+            return{
+                ...prevData, 
+                favorite: !prevData.favorite
+            }
+        })
+    };
+
+    return(
+        <>
+            <div className="house-cards" >
+                <div className="card-content">
+                
+                    <img src= {`../images/${housePic}`} className="card-photo" alt="house-view" />
+                    <div className="price-div">
+                    <span className="price-headings">Monthly rent</span>
+                    <span className="price-headings">List Price</span>
+                    </div>
+            
+                    <div className="prices">
+                        <h2 className="rent">{rent}</h2>
+                        <h2 className="list">{List}</h2>
+                        <img src={`..images/${favIcon}`} alt="fav"
+                        className='favicon'
+                        onClick={toggle} 
+                
+                        />
+                
+                    </div>
+                
+                    <h2 className="size">{size}</h2>
+                    <h2 className="site">{site}</h2>
+                </div>
+            </div>
+        </>
     )
-    })}
-    
-    </>
-)
-
-
-
 }
 
 export default Card;
