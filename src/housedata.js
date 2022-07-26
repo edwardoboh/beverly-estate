59ecc549f99e2f5975aaf73633c061b930cfcccd
jindudimuna <danieldimuna@gmail.com>
2022-07-26 12:38:53 +0100

    card images

---
+++ src/housedata.js
@@ -0,0 +1,46 @@
+// eslint-disable-next-line
+export default [
+    {
+        id: 1,
+        housePic: "3.jpeg",
+        rent: "$11,000",
+        List: "$9,609,000",
+        size: "5 beds 4 baths, 6108 sq ft",
+        site: "300 N Swall Dr #5894, Beverly Hills, CA 90211",
+        favoriteistrue: "favistrue.png",
+        favoriteisfalse: "favisfalse.png"
+    },
+
+    {
+        id: 2,
+        housePic: "4.jpeg",
+        rent: "$10,000",
+        List: "$9,267,000",
+        size: "4 beds 4 baths, 5208 sq ft",
+        site: "250 N Rexford Dr #5594, Beverly Hills, CA 90210",
+        favoriteistrue: "favistrue.png",
+        favoriteisfalse: "favisfalse.png"
+    },
+
+    {
+        id: 3,
+        housePic: "5.jpeg",
+        rent: "$13,300",
+        List: "$12,200,700",
+        size: "5 beds 7 baths, 5500 sq ft",
+        site: "399 San Ysidro Dr #6849, Beverly Hills, CA 90210",
+        favoriteistrue: "favistrue.png",
+        favoriteisfalse: "favisfalse.png"
+    },
+
+    {
+        id: 4,
+        housePic: "6.jpeg",
+        rent: "$15,100",
+        List: "$17,150,000",
+        size: "7 beds 7 baths, 7500 sq ft",
+        site: "9901 San Ysidro Dr #7549, Beverly Hills, CA 90210",
+        favoriteistrue: "favistrue.png",
+        favoriteisfalse: "favisfalse.png"
+    },
+]
